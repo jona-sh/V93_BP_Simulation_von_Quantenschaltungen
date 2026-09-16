a6bc610b2e1c6a1ed5af2d93be469197c50145b8
Felix Gloistein <felixgloistein@gmail.com>
2026-09-16 11:05:04 +0200

    simulation first version

--- src/q_simulator/simulate.py
+++ src/q_simulator/simulate.py
@@ -1,9 +1,20 @@
 import numpy as np
 from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
 
 from .objects import Configuration, Result
 
 
 def simulate(qc: QuantumCircuit, config: Configuration) -> Result:
-    print("test")
-    return Result(counts={}, statevector=np.array([0, 1], dtype=complex))
+    if config.method != "aer_simulator":
+        raise ValueError("Unsupported simulation method")
+
+    qc_to_run = qc.copy()
+    qc_to_run.save_statevector()
+
+    backend = AerSimulator()
+    result = backend.run(qc_to_run, shots=config.number_of_shots).result()
+    counts = result.get_counts(qc_to_run)
+    statevector = np.asarray(result.get_statevector(qc_to_run))
+
+    return Result(counts=counts, statevector=statevector)
