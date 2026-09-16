b3c1c34d64181d69f91e5d76f0fdf65b4b92575c
jona-sh <st182335@stud.uni-stuttgart.de>
2026-09-16 12:17:55 +0200

    setup base for seinsumimplementation

--- src/q_simulator/simulate.py
+++ src/q_simulator/simulate.py
@@ -9,6 +9,8 @@ def simulate(qc: QuantumCircuit, config: Configuration) -> Result:
     match config.method:
         case "default":
             return _simulate_default(qc, config)
+        case "einsum":
+            return _simulate_einsum(qc, config)
         case _:
             raise ValueError(f"Unknown simulation method: {config.method}")
 
@@ -24,3 +26,19 @@ def _simulate_default(qc: QuantumCircuit, config: Configuration) -> Result:
     statevector = np.asarray(result.get_statevector(qc_to_run))
 
     return Result(counts=counts, statevector=statevector)
+
+
+def _simulate_einsum(qc: QuantumCircuit, config: Configuration) -> Result:
+    _apply_unitary(np.array([]), np.array([]), 0)
+    _apply_cx_einsum(np.array([]), 0, 1)
+    return Result(counts={}, statevector=np.array([]))
+
+
+def _apply_unitary(
+    statevector: np.ndarray, operator: np.ndarray, qubit: int
+) -> np.ndarray:
+    return np.array([])
+
+
+def _apply_cx_einsum(statevector: np.ndarray, control: int, target: int) -> np.ndarray:
+    return np.array([])
