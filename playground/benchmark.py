4852e988cb4c451f9b5a5bbbc6693bb90235c811
Felix Gloistein <felixgloistein@gmail.com>
2026-09-17 13:42:20 +0200

    removed unnesessary .copy

--- playground/benchmark.py
+++ playground/benchmark.py
@@ -6,20 +6,7 @@ from qiskit.circuit.random import random_circuit
 
 import q_simulator as qs
 
-# def build_test_circuit() -> QuantumCircuit:
-#     circuit = random_circuit(5, 5)
-#     # rng = np.random.default_rng(1)
-#     # for _ in range(10):
-#     #     gate = rng.choice(["h", "x", "y", "z", "cx"])
-#     #     if gate == "cx":
-#     #         control, target = rng.choice(3, size=2, replace=False)
-#     #         circuit.cx(control, target)
-#     #     else:
-#     #         getattr(circuit, gate)(rng.choice(3))
-#     return circuit
-
-
-circuit = random_circuit(14, 100)
+circuit = random_circuit(18, 50)
 circuit.draw()
 config = qs.Configuration(number_of_shots=2**16)
 
