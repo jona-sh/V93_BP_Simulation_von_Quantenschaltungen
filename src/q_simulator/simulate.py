9c1706b943558f64ddd09c35c309d16ec74fe370
Felix Gloistein <felixgloistein@gmail.com>
2026-09-17 10:36:29 +0200

    added loop simulation function blueprints

--- src/q_simulator/simulate.py
+++ src/q_simulator/simulate.py
@@ -26,6 +26,8 @@ def simulate(qc: QuantumCircuit, config: Configuration) -> Result:
             return _simulate_default(qc, config)
         case "einsum":
             return _simulate_einsum(qc, config)
+        case "loop":
+            return _simulate_loop(qc, config)
         case _:
             raise ValueError(f"Unknown simulation method: {config.method}")
 
@@ -90,6 +92,36 @@ def _simulate_einsum(qc: QuantumCircuit, config: Configuration) -> Result:
     return Result(counts=counts, statevector=final_statevector)
 
 
+def _simulate_loop(qc: QuantumCircuit, config: Configuration) -> Result:
+    transpiled_qc = transpile(qc, optimization_level=0, basis_gates=["u3", "cx"])
+    num_qubits = transpiled_qc.num_qubits
+    statevector = np.zeros(2**num_qubits, dtype=complex)
+    statevector[0] = 1
+    state = np.reshape(statevector, (2,) * num_qubits, order="F")
+
+    for instr in transpiled_qc.data:
+        name = instr.operation.name
+        qubits = [transpiled_qc.find_bit(q).index for q in instr.qubits]
+        if name in {"u", "u3"}:
+            matrix = instr.operation.to_matrix()
+            state = _apply_unitary_loop(state, matrix, qubits[0])
+        elif name == "cx":
+            state = _apply_cx_einsum_loop(state, qubits[0], qubits[1])
+        elif name == "measure" or name == "barrier":
+            pass
+        else:
+            raise ValueError(f"Unsupported gate: {name}")
+
+    final_statevector = np.reshape(state, -1, order="F")
+    probabilities = np.abs(final_statevector) ** 2
+    counts = {}
+    for i, probability in enumerate(probabilities):
+        if probability > 0:
+            counts[f"{i:0{transpiled_qc.num_qubits}b}"] = probability
+
+    return Result(counts=counts, statevector=final_statevector)
+
+
 def _apply_unitary(
     statevector: np.ndarray, operator: np.ndarray, qubit: int
 ) -> np.ndarray:
@@ -123,6 +155,41 @@ def _apply_cx_einsum(statevector: np.ndarray, control: int, target: int) -> np.n
     Returns:
         The updated statevector after applying the CNOT gate  as a tensor.
     """
+    i, j = control, target
+    N = len(statevector.shape)
+
+    assert 0 <= control < N, "qubit index out of range"
+    assert 0 <= target < N, "qubit index out of range"
+    assert control != target, "control and target qubits must be different"
+
+    s = "cdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    I = s[i]
+    J = s[j]
+    if i < j:
+        to = s[:i] + "a" + s[i + 1 : j] + "b" + s[j + 1 : N]
+    else:
+        to = s[:j] + "b" + s[j + 1 : i] + "a" + s[i + 1 : N]
+    psi_new = np.einsum(f"ab{I}{J},{s[:N]}->{to}", CX, statevector)
+    return psi_new
+
+
+def _apply_unitary_loop(
+    statevector: np.ndarray, operator: np.ndarray, qubit: int
+) -> np.ndarray:
+
+    N = len(statevector.shape)
+    assert 0 <= qubit < N, "qubit index out of range"
+    s = "bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    I = s[qubit]
+    to = s[:qubit] + "a" + s[qubit + 1 : N]
+    psi_new = np.einsum(f"a{I},{s[:N]}->{to}", operator, statevector)
+    return psi_new
+
+
+def _apply_cx_einsum_loop(
+    statevector: np.ndarray, control: int, target: int
+) -> np.ndarray:
+
     i, j = control, target
     N = len(statevector.shape)
 
