527a59b77f79c503ec882092b2664c1b576378fb
jona-sh <st182335@stud.uni-stuttgart.de>
2026-09-17 12:07:04 +0200

    implement numba

--- src/q_simulator/simulate.py
+++ src/q_simulator/simulate.py
@@ -1,8 +1,7 @@
 """Simulation backends and tensor-network gate operations."""
 
 import numpy as np
-
-# from numba import jit
+from numba import njit
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 
@@ -30,6 +29,8 @@ def simulate(qc: QuantumCircuit, config: Configuration) -> Result:
             return _simulate_einsum(qc, config)
         case "loop":
             return _simulate_loop(qc, config)
+        case "numba":
+            return _simulate_numba(qc, config)
         case _:
             raise ValueError(f"Unknown simulation method: {config.method}")
 
@@ -47,7 +48,9 @@ def _simulate_default(qc: QuantumCircuit, config: Configuration) -> Result:
     qc_to_run = qc.copy()
     qc_to_run.save_statevector()
 
-    backend = AerSimulator()
+    backend = AerSimulator(
+        fusion_enable=False, max_parallel_threads=1
+    )  # to avoid race conditions in parallel execution
     result = backend.run(qc_to_run, shots=config.number_of_shots).result()
     counts = result.get_counts(qc_to_run)
     statevector = np.asarray(result.get_statevector(qc_to_run))
@@ -124,6 +127,36 @@ def _simulate_loop(qc: QuantumCircuit, config: Configuration) -> Result:
     return Result(counts=counts, statevector=final_statevector)
 
 
+def _simulate_numba(qc: QuantumCircuit, config: Configuration) -> Result:
+    transpiled_qc = transpile(qc, optimization_level=0, basis_gates=["u3", "cx"])
+    num_qubits = transpiled_qc.num_qubits
+    statevector = np.zeros(2**num_qubits, dtype=complex)
+    statevector[0] = 1
+    state = statevector
+
+    for instr in transpiled_qc.data:
+        name = instr.operation.name
+        qubits = [transpiled_qc.find_bit(q).index for q in instr.qubits]
+        if name in {"u", "u3"}:
+            matrix = instr.operation.to_matrix()
+            state = _apply_unitary_numba(state, matrix, qubits[0])
+        elif name == "cx":
+            state = _apply_cx_numba(state, qubits[0], qubits[1])
+        elif name == "measure" or name == "barrier":
+            pass
+        else:
+            raise ValueError(f"Unsupported gate: {name}")
+
+    final_statevector = state
+    probabilities = np.abs(final_statevector) ** 2
+    counts = {}
+    for i, probability in enumerate(probabilities):
+        if probability > 0:
+            counts[f"{i:0{transpiled_qc.num_qubits}b}"] = probability
+
+    return Result(counts=counts, statevector=final_statevector)
+
+
 def _apply_unitary(
     statevector: np.ndarray, operator: np.ndarray, qubit: int
 ) -> np.ndarray:
@@ -175,7 +208,6 @@ def _apply_cx_einsum(statevector: np.ndarray, control: int, target: int) -> np.n
     return psi_new
 
 
-# @jit
 def _apply_unitary_loop(
     statevector: np.ndarray, operator: np.ndarray, qubit: int
 ) -> np.ndarray:
@@ -210,8 +242,71 @@ def _apply_unitary_loop(
     return psi_new
 
 
-# @jit
 def _apply_cx_loop(statevector: np.ndarray, control: int, target: int) -> np.ndarray:
+    """Apply a CNOT gate to the statevector using the fast loop method which was implemented for performance reasons.
+
+    Args:
+        statevector: The current statevector of the quantum system as a tensor.
+        control: The index of the control qubit.
+        target: The index of the target qubit.
+
+    Returns:
+        The updated statevector after applying the CNOT gate  as a tensor.
+    """
+    N = int(np.log2(statevector.size))
+
+    assert 0 <= control < N, "qubit index out of range"
+    assert 0 <= target < N, "qubit index out of range"
+    assert control != target, "control and target qubits must be different"
+
+    psi_new = statevector.copy()
+    for r in range(2**target):
+        for s in range(2 ** (N - target - 1)):
+            index = r + s * 2 ** (target + 1)
+            partner = index + 2**target
+            if (index >> control) & 1:
+                psi_new[index] = statevector[partner]
+                psi_new[partner] = statevector[index]
+    return psi_new
+
+
+@njit
+def _apply_unitary_numba(
+    statevector: np.ndarray, operator: np.ndarray, qubit: int
+) -> np.ndarray:
+    """Apply a single-qubit unitary operator to a specific qubit in the statevector using the fast loop method which was implemented for performance reasons.
+
+    Args:
+        statevector: The current statevector of the quantum system as a tensor.
+        operator: The unitary operator to apply.
+        qubit: The index of the qubit to apply the operator to.
+
+    Returns:
+        The updated statevector after applying the operator as a tensor.
+    """
+    psi_new = np.copy(statevector)
+    N = int(np.log2(statevector.size))
+    assert 0 <= qubit < N, "qubit index out of range"
+
+    psi_new = statevector.copy()
+    for r in range(2**qubit):
+        for s in range(2 ** (N - qubit - 1)):
+            index = r + s * 2 ** (qubit + 1)
+            partner = index + 2**qubit
+
+            psi_new[index] = (
+                operator[0, 0] * statevector[index]
+                + operator[0, 1] * statevector[partner]
+            )
+            psi_new[partner] = (
+                operator[1, 0] * statevector[index]
+                + operator[1, 1] * statevector[partner]
+            )
+    return psi_new
+
+
+@njit
+def _apply_cx_numba(statevector: np.ndarray, control: int, target: int) -> np.ndarray:
     """Apply a CNOT gate to the statevector using the fast loop method which was implemented for performance reasons.
 
     Args:
--- tests/test_random_circuit.py
+++ tests/test_random_circuit.py
@@ -33,7 +33,7 @@ def test_simulate_default_1():
 
 
 @pytest.mark.parametrize("seed", [1, 2, 3, 42, 1234])
-@pytest.mark.parametrize("simulation_method", ["default", "einsum", "loop"])
+@pytest.mark.parametrize("simulation_method", ["default", "einsum", "loop", "numba"])
 def test_random_circuit(seed, simulation_method):
     """test the simulation of a random circuit with 3 qubits"""
     tol = 400
