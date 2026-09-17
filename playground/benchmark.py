7d2389722a218e4c1dfed736107695d1b76ad929
jona-sh <st182335@stud.uni-stuttgart.de>
2026-09-17 16:01:44 +0200

    update performanceplots

--- playground/benchmark.py
+++ playground/benchmark.py
@@ -20,8 +20,8 @@ data_depths: dict[str, list[float]] = {
     "loop": [],
     "numba": [],
 }
-qubits_iterate = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-depths_iterate = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+qubits_iterate = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 20]
+depths_iterate = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 750]
 
 for qubits in qubits_iterate:
     print(f"Simulating circuit with {qubits} qubits and depth 50")
@@ -29,6 +29,10 @@ for qubits in qubits_iterate:
     circuit.draw()
     config = qs.Configuration(number_of_shots=2**16)
     for method in ["default", "einsum", "loop", "numba"]:
+        if method == "loop" and qubits >= 13:
+            continue
+        if method == "einsum" and qubits >= 17:
+            continue
         config.method = method
         start = time.time()
         qs.simulate(circuit.copy(), config)
@@ -42,6 +46,10 @@ for depth in depths_iterate:
     circuit.draw()
     config = qs.Configuration(number_of_shots=2**16)
     for method in ["default", "einsum", "loop", "numba"]:
+        if method == "einsum" and depth > 600:
+            continue
+        if method == "loop" and depth > 400:
+            continue
         config.method = method
         start = time.time()
         qs.simulate(circuit.copy(), config)
@@ -52,8 +60,8 @@ for depth in depths_iterate:
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.plot(qubits_iterate, data_qubits["default"], label="default")
-plt.plot(qubits_iterate, data_qubits["einsum"], label="einsum")
-plt.plot(qubits_iterate, data_qubits["loop"], label="loop")
+plt.plot([q for q in qubits_iterate if q < 17], data_qubits["einsum"], label="einsum")
+plt.plot([q for q in qubits_iterate if q < 13], data_qubits["loop"], label="loop")
 plt.plot(qubits_iterate, data_qubits["numba"], label="numba")
 plt.xlabel("Qubits")
 plt.ylabel("Time (s)")
@@ -63,8 +71,8 @@ plt.legend()
 
 plt.subplot(1, 2, 2)
 plt.plot(depths_iterate, data_depths["default"], label="default")
-plt.plot(depths_iterate, data_depths["einsum"], label="einsum")
-plt.plot(depths_iterate, data_depths["loop"], label="loop")
+plt.plot([d for d in depths_iterate if d <= 600], data_depths["einsum"], label="einsum")
+plt.plot([d for d in depths_iterate if d <= 400], data_depths["loop"], label="loop")
 plt.plot(depths_iterate, data_depths["numba"], label="numba")
 plt.xlabel("Depth")
 plt.ylabel("Time (s)")
